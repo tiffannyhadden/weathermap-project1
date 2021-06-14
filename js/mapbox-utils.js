259f0a450134f1c883a4ded9f3d2f0252bf83ed8
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-14 10:42:55 -0700

    adds changes

--- js/mapbox-utils.js
+++ js/mapbox-utils.js
@@ -9,6 +9,7 @@ let map = new mapboxgl.Map({
 map.addControl(new mapboxgl.NavigationControl());
 
 
+
 var popup = new mapboxgl.Popup()
     .setHTML("<h5>San Antonio, Texas, United States</h5>")
     .setMaxWidth("200px");
@@ -24,6 +25,26 @@ var marker = new mapboxgl.Marker({
 marker.on('dragend', function (){
     let coords = marker.getLngLat().toArray().reverse();
     fetchForecast(coords);
+});
+
+let geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    types: 'country,region,place,postcode,locality,neighborhood',
+    mapboxgl: mapboxgl,
+    marker: false
+
+});
+geocoder.onAdd(map);
+geocoder.addTo('#geocoder');
+
+
+// Get the geocoder results container.
+let results = document.getElementById('result');
+
+// Add geocoder result to container.
+geocoder.on('result', function (e) {
+    results.innerText = e.result.place_name;
+    marker.setLngLat(e.result.geometry.coordinates)
 });
 
 
